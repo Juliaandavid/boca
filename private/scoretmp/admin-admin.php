46c2e4d86536f5549e676fac8701aaa0b42c8cc4
Julian Izquierdo <juliandizquierdog@gmail.com>
2021-09-28 21:33:49 -0500

    Primer commit

---
+++ private/scoretmp/admin-admin.php
@@ -0,0 +1,12 @@
+<!-- 1632878119 --> <?php exit; ?>
+<script language="JavaScript" src="/boca/hide.js"></script>
+<br>
+<br>
+<table id="myscoretable" width="100%" border=1>
+ <tr>
+  <td><b>#</b></td>
+  <td><b>User/Site</b></td>
+  <td><b>Name</b></td>
+<td><b>Total</b></td>
+</tr>
+</table><br><center><b><font color="#ff0000">SCOREBOARD IS EMPTY</font></b></center>
